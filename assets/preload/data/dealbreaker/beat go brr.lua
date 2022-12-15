000da7877587c0f735211388cb74bb9442e80029
Xkxksixickzociv <119426594+Xkxksixickzociv@users.noreply.github.com>
2022-12-15 22:36:18 +0200

    Add files via upload

---
+++ assets/preload/data/dealbreaker/beat go brr.lua
@@ -0,0 +1,8 @@
+function onStepHit()
+    if curBeat > 0 then -- THIISSSSSSSSSSS SJAJFGSDGHFKSJHADFHJLSADHFAKJDSBJHFJDBJHFHJSDFBSDJKFBNSDJ CIT6Y CITYY CITY CITY
+		doTweenX('CITY GO BRRR', 'neo_city', -1100+3800, 160, linear);
+        doTweenX('CITY GO BRRR2', 'neo_city2', 200, 160, linear);
+        doTweenX('CITY GO BRRR3', 'neo_amongus', -220+2891, 160, linear);
+        doTweenX('CITY GO BRRR4', 'neo_amongus2', 200, 160, linear);
+	end
+end
